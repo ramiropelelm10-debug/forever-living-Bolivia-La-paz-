5390e7a1f4226f0c049b929b503a25d691d69f2d
barberia10 <ramiropelelm10@gmail.com>
2026-06-15 17:58:00 -0400

    Ruta de migración secreta

--- forever-api/routes/web.php
+++ forever-api/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan; // <-- IMPORTANTE: Traemos el motor de comandos
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// 🔥 EL TRUCO SECRETO PARA ACTIVAR LA BASE DE DATOS EN RENDER FREE 🔥
+Route::get('/instalar-bd', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return '¡Base de datos creada con éxito, ingeniero!';
 });
